359f40cb32803d4d51d6cd72895fdc7922579d71
Tyler Stuart <tstuart@1on1.com>
2013-03-20 09:16:43 -0600

    Updated frontend for the new authentication system
    Added simple_form
    Updated several forms to bootstrap/simple_form
    Added login/logout register/ profile links to our nav
    added a collection select to our status form

--- db/migrate/20130313191907_create_statuses.rb
+++ db/migrate/20130313191907_create_statuses.rb
@@ -1,8 +1,10 @@
 class CreateStatuses < ActiveRecord::Migration
   def change
-  	  add_column :statuses, :user_id, :integer
-  	  add_index :statuses, :user_id
-  	  remove_column :statuses, :name
+    create_table :statuses do |t|
+      t.string :name
+      t.text :content
+
+      t.timestamps
     end
   end
 end
